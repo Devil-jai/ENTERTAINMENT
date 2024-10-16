521a9e29257c9799c3cb68aaf0ebc0026a0cf9c6
Devil-jai <140802260+Devil-jai@users.noreply.github.com>
2024-10-16 16:03:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,14 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://entertainment-ms1g.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 // Define routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/movie", protectRoute, movieRoutes);
