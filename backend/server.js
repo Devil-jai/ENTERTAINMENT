cf659bbcdc936f339d7f89c91503ccfc1f99967e
Devil-jai <140802260+Devil-jai@users.noreply.github.com>
2024-10-16 17:31:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.json()); // Middleware to parse JSON bodies
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "https://entertainment-ms1g.vercel.app",
+    origin: "https://entertainment-1-jcso.onrender.com",
     methods: ['GET', 'POST', 'PUT', 'DELETE' , 'OPTIONS'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'], 
