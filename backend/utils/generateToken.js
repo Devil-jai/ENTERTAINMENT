2bb0570653ca2928499b8f0a35ba9b84ad65cee8
Devil-jai <140802260+Devil-jai@users.noreply.github.com>
2024-10-17 18:09:18 +0530

    Update generateToken.js

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -5,10 +5,11 @@ export const generateTokenAndSetCookie = (userId, res) => {
     const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
 
     res.cookie("jwt-entertainment", token, {
-        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
         httpOnly: true,
-        sameSite: "lax",  // Changed from 'strict' to 'lax' for cross-origin requests
-        secure: ENV_VARS.NODE_ENV !== "development",  // Use 'secure' cookie in production
+    secure: process.env.NODE_ENV === 'development',
+    sameSite: "None",
+        maxAge: 15 * 24 * 60 * 60 * 1000, // 15 days
+   
     });
 
     return token;
