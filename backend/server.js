f2144c38a4287583f56434c49be03ed31f2de0bc
Devil-jai <140802260+Devil-jai@users.noreply.github.com>
2024-10-17 17:46:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://entertainment-1-jcso.onrender.com');
+    res.header('Access-Control-Allow-Origin', 'https://inspiring-cendol-af7025.netlify.app');
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE , OPTIONS' );
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
